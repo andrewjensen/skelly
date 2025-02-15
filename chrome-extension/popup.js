9ce11df8e6d8f4f2a54dea2e7a5004c00eea04c4
Andrew Jensen <andrewjensen90@gmail.com>
2025-02-15 16:41:55 -0700

    Add status banner

--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -1,4 +1,37 @@
+const STATUSES = {
+  INITIAL: {
+    cssClass: "status-initial",
+    message: "Initialized",
+  },
+  CONNECTED: {
+    cssClass: "status-connected",
+    message: "Connected to Skelly",
+  },
+  DISCONNECTED: {
+    cssClass: "status-disconnected",
+    message: "Disconnected from Skelly",
+  },
+  CONNECTING: {
+    cssClass: "status-pending",
+    message: "Connecting to Skelly...",
+  },
+  RENDER_LOADING: {
+    cssClass: "status-pending",
+    message: "Sending...",
+  },
+  RENDER_SUCCESS: {
+    cssClass: "status-success",
+    message: "Sent successfully",
+  },
+  RENDER_ERROR: {
+    cssClass: "status-error",
+    message: "Failed to send",
+  },
+};
+
 let skellyHost = null;
+
+let appStatus = STATUSES.INITIAL;
 
 function main() {
   skellyHost = localStorage.getItem("skellyHost");
@@ -6,21 +39,40 @@ function main() {
     document.getElementById("skelly-host").value = skellyHost;
   }
 
+  setStatus(STATUSES.INITIAL);
+
   document.getElementById("send-to-skelly").addEventListener("click", handleClickSendToSkelly);
 
   document.getElementById("save-skelly-host").addEventListener("click", handleClickSaveSkellyHost);
 
   chrome.runtime.onMessage.addListener(handleMessage);
+
 }
 
 main();
+
+function setStatus(newStatus) {
+  console.log("Setting status to", newStatus);
+
+  appStatus = newStatus;
+  const statusBanner = document.getElementById("status-banner");
+  statusBanner.textContent = appStatus.message;
+  statusBanner.className = appStatus.cssClass;
+}
 
 async function handleMessage(message, sender, sendResponse) {
   if (message.type === "send-to-skelly") {
     console.log("Received send-to-skelly message", message);
     const { pageHtml, pageUrl } = message;
-    await requestRender(pageHtml, pageUrl);
-    sendResponse({ status: "ok" });
+    try {
+      await requestRender(pageHtml, pageUrl);
+      sendResponse({ status: "ok" });
+      setStatus(STATUSES.RENDER_SUCCESS);
+    } catch (e) {
+      console.error("Error sending to Skelly", e);
+      sendResponse({ status: "error" });
+      setStatus(STATUSES.RENDER_ERROR);
+    }
   }
 }
 
@@ -45,6 +97,8 @@ function handleClickSaveSkellyHost() {
 
 function handleClickSendToSkelly() {
   console.log("Clicked Send to Skelly");
+  setStatus(STATUSES.RENDER_LOADING);
+
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     const currentTabId = tabs[0].id;
     console.log("Current tab ID:", currentTabId);
